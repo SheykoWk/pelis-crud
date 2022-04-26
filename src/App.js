b88af74622699dba646613149375bebea596ba25
sheyko <sheyko.wk@gmail.com>
2022-04-25 22:03:50 -0500

    fix bugs

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
 
 
   useEffect(() => {
-    if(newMovie.name){
+    if(newMovie.name ){
       createNewMovie(newMovie)
         .then((res) => {
           console.log(res.data)
